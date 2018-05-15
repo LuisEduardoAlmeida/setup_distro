eec30bed3fc63c59660e3fe812c6229b29fe4f3f
LuisEduardoAlmeida <leduardoalmeida@hotmail.com>
2018-05-15 16:56:14 -0300

    Update 16_04.sh

--- ubuntu/16_04.sh
+++ ubuntu/16_04.sh
@@ -14,6 +14,16 @@ sudo apt update && sudo apt upgrade -y
 # echo "========================================================================"
 # echo "# INSTALANDO UBUNTU RESTRICTED EXTRAS"
 # echo "========================================================================"
+sudo apt update && sudo apt curl -y
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+# echo
+# echo "========================================================================"
+# echo "# INSTALANDO UBUNTU RESTRICTED EXTRAS"
+# echo "========================================================================"
 sudo apt update && sudo apt install ubuntu-restricted-extras -y
 
 # echo
@@ -44,18 +54,29 @@ git config --global user.email "$git_user_email"
 # echo "========================================================================"
 # echo "# INSTALANDO DEPENDENCIAS DO RVM"
 # echo "========================================================================"
+sudo apt-get update
 sudo apt install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev \
-                        libsqlite3-dev sqlite3 libxml2-dev libcurl4-openssl-dev python-software-properties \
+                        libsql libsqlite3-dev sqlite3 libxml2-dev libcurl4-openssl-dev python-software-properties \
                         libffi-dev nodejs libgdbm-dev libncurses5-dev automake libtool bison libxslt1-dev
 
 # echo
 # echo "======================================================================="
 # echo "# INSTALANDO RVM"
 # echo "========================================================================"
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s
-source ~/.rvm/scripts/rvm
+sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
 rvm requirements
+source ~/.rvm/scripts/rvm
+rvm install 2.1.2
+rvm use 2.5.1 --default
+ruby -v
+
+
+#gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+#\curl -sSL https://get.rvm.io | bash -s
+#source ~/.rvm/scripts/rvm
+#rvm requirements
 
 # echo
 # echo "========================================================================"
